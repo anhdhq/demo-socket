c86fe4ad4335d896e174fd8390620cdc09161663
hieu.nt6 <hieu.nt6@cmctelecom.vn>
2022-03-28 17:44:17 +0700

    hieu.nt

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+// Setup basic express server
 const express = require("express");
-const socket = require("socket.io");
-
-// App setup
-const PORT = 3000;
 const app = express();
-const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = 3000;
+const cors = require("cors");
+
+server.listen(port, () => {
+  console.log("Server listening at port %d", port);
 });
 
-// Static files
-app.use(express.static("public"));
+app.use(cors({
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 
-// Socket setup
-const io = socket(server);
-
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
+io.on("connection", (socket) => {
+  console.log("connected", socket.id);
 });
